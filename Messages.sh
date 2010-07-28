5e7a947a6a4b7359c379707f192018bfbaf00905
Albert Astals Cid <tsdgeos@terra.es>
2010-07-28 18:58:55 +0000

    take into account the rc.cpp in the xgettext line otherwise it won't get used remove the rm rc.cpp line as it's not needed at all
    svn path=/trunk/playground/sysadmin/muon/; revision=1156237

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,3 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find src/ -name \*.rc` >> rc.cpp
-$XGETTEXT `find src/ -name \*.cpp` -o $podir/muon.pot
-rm -f rc.cpp
+$XGETTEXT rc.cpp `find src/ -name \*.cpp` -o $podir/muon.pot
