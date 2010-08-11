9d03e79b0dc492eb5c56eddd3dee34038291ae82
Jonathan Michael Thomas <echidnaman@kubuntu.org>
2010-08-11 14:38:52 +0000

    Version bump + codename for 1.0
    svn path=/trunk/playground/sysadmin/muon/; revision=1162164

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@
 static const char description[] =
     I18N_NOOP("A package manager");
 
-static const char version[] = "0.5.0 (1.0 RC)";
+static const char version[] = "1.0 \"Ambivalent Atraxi\"";
 
 int main(int argc, char **argv)
 {
