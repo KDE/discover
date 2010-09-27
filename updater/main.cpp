84978185ab34246d516f771629f009c4812eec55
Jonathan Michael Thomas <echidnaman@kubuntu.org>
2010-09-27 20:48:25 +0000

    Use the muon catalog as the main l10n catalog
    svn path=/trunk/extragear/sysadmin/muon/; revision=1180390

--- updater/main.cpp
+++ updater/main.cpp
@@ -33,7 +33,7 @@ static const char version[] = "1.0.1 \"Ambivalent Atraxi\"";
 
 int main(int argc, char **argv)
 {
-    KAboutData about("muon-updater", 0, ki18n("Muon Update Manager"), version, ki18n(description),
+    KAboutData about("muon-updater", "muon", ki18n("Muon Update Manager"), version, ki18n(description),
                      KAboutData::License_GPL, ki18n("©2010 Jonathan Thomas"), KLocalizedString(), 0);
     about.addAuthor(ki18n("Jonathan Thomas"), KLocalizedString(), "echidnaman@kubuntu.org");
     about.setProgramIconName("system-software-update");
