f68e011fcdac0b4e7ad8762bcfd7ee71657ef78e
Pino Toscano <pino@kde.org>
2011-01-18 10:26:44 +0100

    remove muonrc.cpp and notifierrc.cpp instead of rc.cpp

--- Messages.sh
+++ Messages.sh
@@ -7,5 +7,6 @@ $XGETTEXT muonrc.cpp `find muon -name \*.cpp` -o $podir/muon.pot
 $XGETTEXT updaterrc.cpp `find updater -name \*.cpp` -o $podir/muon-updater.pot
 $XGETTEXT `find installer -name \*.cpp` -o $podir/muon-installer.pot
 $XGETTEXT notifierrc.cpp `find kded/ -name \*.cpp` -o $podir/muon-notifier.pot
-rm -f rc.cpp
+rm -f muonrc.cpp
+rm -f notifierrc.cpp
 rm -f updaterrc.cpp
