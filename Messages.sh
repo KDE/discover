952c8cfe5dbee010d4dea54f948d205fc4d13d17
Jonathan Thomas <echidnaman@kubuntu.org>
2011-01-19 09:27:07 -0500

    Include the rc.cpp file with the translation credit strings into our .pots. Thanks to Chusslove for the patch I will backport this to the 1.1 branch CCMAIL:kde-i18n-doc@kde.org

--- Messages.sh
+++ Messages.sh
@@ -3,10 +3,10 @@ $EXTRACTRC `find muon/ -name \*.rc` >> muonrc.cpp
 $EXTRACTRC `find updater/ -name \*.rc` >> updaterrc.cpp
 $EXTRACTRC `find kded/ -name \*.rc` >> notifierrc.cpp
 $XGETTEXT `find libmuon -name \*.cpp` -o $podir/libmuon.pot
-$XGETTEXT muonrc.cpp `find muon -name \*.cpp` -o $podir/muon.pot
-$XGETTEXT updaterrc.cpp `find updater -name \*.cpp` -o $podir/muon-updater.pot
-$XGETTEXT `find installer -name \*.cpp` -o $podir/muon-installer.pot
-$XGETTEXT notifierrc.cpp `find kded/ -name \*.cpp` -o $podir/muon-notifier.pot
+$XGETTEXT rc.cpp muonrc.cpp `find muon -name \*.cpp` -o $podir/muon.pot
+$XGETTEXT rc.cpp updaterrc.cpp `find updater -name \*.cpp` -o $podir/muon-updater.pot
+$XGETTEXT rc.cpp `find installer -name \*.cpp` -o $podir/muon-installer.pot
+$XGETTEXT rc.cpp notifierrc.cpp `find kded/ -name \*.cpp` -o $podir/muon-notifier.pot
 rm -f muonrc.cpp
 rm -f notifierrc.cpp
 rm -f updaterrc.cpp
