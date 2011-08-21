7604c5bc56a000599a43763643e536b4fb99aca9
Jonathan Thomas <echidnaman@kubuntu.org>
2011-08-20 20:34:41 -0400

    Stubs for the new updater UI.

---
+++ updater/MainWindow.h
@@ -0,0 +1,19 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+// Own includes
+#include "../libmuon/MuonMainWindow.h"
+
+class MainWindow : public MuonMainWindow
+{
+    Q_OBJECT
+public:
+    explicit MainWindow(QWidget *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // MAINWINDOW_H
