61bb4b604eb39175a3b4f75f74e848e8731a01ed
Jonathan Thomas <echidnaman@kubuntu.org>
2011-12-21 20:16:50 -0500

    Version bump for 1.2.80

--- muon/main.cpp
+++ muon/main.cpp
@@ -29,7 +29,7 @@
 static const char description[] =
     I18N_NOOP("A package manager");
 
-static const char version[] = "1.2.65 \"Daring Dalek\"";
+static const char version[] = "1.2.80 \"Daring Dalek\"";
 
 int main(int argc, char **argv)
 {
