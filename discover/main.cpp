a02bf4fc1993885428019d5807710e15e8c6b543
Jonathan Thomas <echidnaman@kubuntu.org>
2012-10-28 15:24:03 -0400

    Version bump for 1.9.60 (2.0 alpha)

--- discover/main.cpp
+++ discover/main.cpp
@@ -28,7 +28,7 @@
 static const char description[] =
     I18N_NOOP("An application discoverer");
 
-static const char version[] = "1.4.0";
+static const char version[] = "1.9.60";
 
 int main(int argc, char** argv)
 {
