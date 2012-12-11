8889dcbb1d69ecb4b514bfb9720be9c48157e141
Aleix Pol <aleixpol@kde.org>
2012-12-11 18:26:28 +0100

    more moving code around
    Move the tests with the backends. We don't want them tied to the application,
    specially since they're mostly apt-dependent.
    Make a libmuonapt declarative plugin

---
+++ libmuonapt/declarative/MuonAptDeclarativePlugin.h
@@ -0,0 +1,33 @@
+/*
+ *   Copyright (C) 2012 Aleix Pol Gonzalez <aleixpol@blue-systems.com>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library/Lesser General Public License
+ *   version 2, or (at your option) any later version, as published by the
+ *   Free Software Foundation
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library/Lesser General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef MUONAPTDECLARATIVEPLUGIN_H
+#define MUONAPTDECLARATIVEPLUGIN_H
+
+#include <QDeclarativeExtensionPlugin>
+
+class MuonAptDeclarativePlugin : public QDeclarativeExtensionPlugin
+{
+    public:
+        virtual void registerTypes(const char* uri);
+};
+
+Q_EXPORT_PLUGIN2(muonaptdeclarativeplugin, MuonAptDeclarativePlugin);
+
+#endif // MUONAPTDECLARATIVEPLUGIN_H
