1df6f0ea22fc737fc834c783173c1128d52c1578
Jonathan Thomas <echidnaman@kubuntu.org>
2013-01-24 12:22:55 -0500

    Bump versions for 1.6.80 (2.0 beta)

--- installer/main.cpp
+++ installer/main.cpp
@@ -30,7 +30,7 @@
 static const char description[] =
     I18N_NOOP("An application manager");
 
-static const char version[] = "1.9.65";
+static const char version[] = "1.9.80";
 
 int main(int argc, char **argv)
 {
