fd1c98b98d68194ad398bc8db44a1b284647a514
Aleix Pol <aleixpol@kde.org>
2013-01-29 17:05:46 +0100

    Create a dummy backend for testing purposes
    This will make it much easier to test applications with backends in some
    cases.
    Its compilation has to be explicitly enabled through the BUILD_DummyBackend
    variable in the CMake cache.

---
+++ libmuon/backends/DummyBackend/DummyBackend.cpp
@@ -0,0 +1,123 @@
+/***************************************************************************
+ *   Copyright © 2013 Aleix Pol Gonzalez <aleixpol@blue-systems.com>       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU General Public License as        *
+ *   published by the Free Software Foundation; either version 2 of        *
+ *   the License or (at your option) version 3 or any later version        *
+ *   accepted by the membership of KDE e.V. (or its successor approved     *
+ *   by the membership of KDE e.V.), which shall act as a proxy            *
+ *   defined in Section 14 of version 3 of the license.                    *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ ***************************************************************************/
+
+#include "DummyBackend.h"
+#include "DummyResource.h"
+
+#include <KAboutData>
+#include <KLocalizedString>
+#include <KPluginFactory>
+#include <QDebug>
+
+K_PLUGIN_FACTORY(MuonDummyBackendFactory, registerPlugin<DummyBackend>(); )
+K_EXPORT_PLUGIN(MuonDummyBackendFactory(KAboutData("muon-dummybackend","muon-dummybackend",ki18n("Dummy Backend"),"0.1",ki18n("Dummy backend to test muon frontends"), KAboutData::License_GPL)))
+
+DummyBackend::DummyBackend(QObject* parent, const QVariantList&)
+    : AbstractResourcesBackend(parent)
+    , m_updater(0)
+{
+    for(int i=0; i<32; i++) {
+        QString name = "alalala"+QString::number(i);
+        DummyResource* res = new DummyResource(name, this);
+        res->setState(AbstractResource::State(1+(i%3)));
+        m_resources.insert(name, res);
+    }
+    
+    QMetaObject::invokeMethod(this, "backendReady", Qt::QueuedConnection);
+}
+
+QVector<AbstractResource*> DummyBackend::allResources() const
+{
+    QVector<AbstractResource*> ret;
+    ret.reserve(m_resources.size());
+    foreach(AbstractResource* res, m_resources) {
+        ret += res;
+    }
+    return ret;
+}
+
+int DummyBackend::updatesCount() const
+{
+    return upgradeablePackages().count();
+}
+
+QList<AbstractResource*> DummyBackend::upgradeablePackages() const
+{
+    QList<AbstractResource*> updates;
+    foreach(AbstractResource* res, m_resources) {
+        if(res->state()==AbstractResource::Upgradeable)
+            updates += res;
+    }
+    qDebug() << "falalala" << updates;
+    return updates;
+}
+
+AbstractResource* DummyBackend::resourceByPackageName(const QString& name) const
+{
+    return m_resources.value(name);
+}
+
+QStringList DummyBackend::searchPackageName(const QString& searchText)
+{
+    QStringList ret;
+    foreach(AbstractResource* r, m_resources) {
+        if(r->name().contains(searchText) || r->comment().contains(searchText))
+            ret += r->packageName();
+    }
+    return ret;
+}
+
+AbstractBackendUpdater* DummyBackend::backendUpdater() const
+{
+    return m_updater;
+}
+
+AbstractReviewsBackend* DummyBackend::reviewsBackend() const
+{
+    return 0;
+}
+
+QPair<TransactionStateTransition, Transaction*> DummyBackend::currentTransactionState() const
+{
+    return QPair<TransactionStateTransition, Transaction*>(FinishedCommitting, 0);
+}
+
+QList<Transaction*> DummyBackend::transactions() const
+{
+    return QList<Transaction*>();
+}
+
+void DummyBackend::installApplication(AbstractResource* app, const QHash< QString, bool >&)
+{
+    installApplication(app);
+}
+
+void DummyBackend::installApplication(AbstractResource* app)
+{
+    qobject_cast<DummyResource*>(app)->setState(AbstractResource::Installed);
+}
+
+void DummyBackend::removeApplication(AbstractResource* app)
+{
+    qobject_cast<DummyResource*>(app)->setState(AbstractResource::None);
+}
+
+void DummyBackend::cancelTransaction(AbstractResource*)
+{}
