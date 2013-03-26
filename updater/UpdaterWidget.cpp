KDE/discover

commit 1d22f1226c1427339136ce221963d1e1a3af0f62

Don't add a connection each time the backend reloads. Fixes multiple calls to populateUpdateModel()

updater/UpdaterWidget.cpp
@@ -82,6 +82,8 @@ UpdaterWidget::UpdaterWidget(QWidget *parent) :
 
     connect(m_updateModel, SIGNAL(checkApps(QList<AbstractResource*>,bool)),
             this, SLOT(checkApps(QList<AbstractResource*>,bool)));
+    connect(m_appsBackend, SIGNAL(reloadFinished()),
+            this, SLOT(populateUpdateModel()));
 
     m_updateView = new QTreeView(page1);
     m_updateView->setAlternatingRowColors(true);
@@ -148,8 +150,6 @@ void UpdaterWidget::reload()
     QMetaObject::invokeMethod(m_appsBackend, "reload", Qt::QueuedConnection);
 
     setCurrentIndex(0);
-    connect(m_appsBackend, SIGNAL(reloadFinished()),
-            this, SLOT(populateUpdateModel()));
 }
 
 void UpdaterWidget::populateUpdateModel()

commit 569191f4976cb4ecbfa10f1640a814cf994ad8cf

Fix a crash on startup caused by 1d22f122

updater/UpdaterWidget.cpp
@@ -82,8 +82,6 @@ UpdaterWidget::UpdaterWidget(QWidget *parent) :
 
     connect(m_updateModel, SIGNAL(checkApps(QList<AbstractResource*>,bool)),
             this, SLOT(checkApps(QList<AbstractResource*>,bool)));
-    connect(m_appsBackend, SIGNAL(reloadFinished()),
-            this, SLOT(populateUpdateModel()));
 
     m_updateView = new QTreeView(page1);
     m_updateView->setAlternatingRowColors(true);
@@ -137,6 +135,8 @@ UpdaterWidget::UpdaterWidget(QWidget *parent) :
 void UpdaterWidget::setBackend(AbstractResourcesBackend *backend)
 {
     m_appsBackend = backend;
+    connect(m_appsBackend, SIGNAL(reloadFinished()),
+            this, SLOT(populateUpdateModel()));
     m_backend = qobject_cast<QApt::Backend*>(backend->property("backend").value<QObject*>());
     connect(m_backend, SIGNAL(packageChanged()),
             m_updateModel, SLOT(packageChanged()));