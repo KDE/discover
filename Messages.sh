8b838c38b07035f9252d34700881b7c7077a6cda
Jonathan Thomas <echidnaman@kubuntu.org>
2013-04-18 10:46:14 -0400

    Fix Messages.sh. (eek. very, very sorry)

--- Messages.sh
+++ Messages.sh
@@ -4,8 +4,8 @@ $EXTRACTRC `find muon/ -name \*.rc` >> muonrc.cpp
 $EXTRACTRC `find updater/ -name \*.rc` >> updaterrc.cpp
 $EXTRACTRC `find kded/ -name \*.rc` >> notifierrc.cpp
 $EXTRACTRC `find libmuonapt/ -name \*.rc` >> libmuonapt.cpp
-$EXTRACTRC --context="Category" --tag-group=none --tag=Name libmuon/categories.xml >> categoriesxml.cpp
-$XGETTEXT categoriesxml.cpp libmuonapt.cpp `find libmuon -name \*.cpp` -o $podir/libmuon.pot
+$EXTRACTRC --context="Category" --tag-group=none --tag=Name `find libmuon -name categories.xml` >> categoriesxml.cpp
+$XGETTEXT categoriesxml.cpp libmuonapt.cpp `find libmuon -name \*.cpp` `find libmuonapt -name \*.cpp` -o $podir/libmuon.pot
 $XGETTEXT rc.cpp muonrc.cpp `find muon -name \*.cpp` -o $podir/muon.pot
 $XGETTEXT rc.cpp updaterrc.cpp `find updater -name \*.cpp` -o $podir/muon-updater.pot
 $XGETTEXT rc.cpp `find installer -name \*.cpp` -o $podir/muon-installer.pot
