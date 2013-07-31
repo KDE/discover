0422d76ec989d69cf42e4c3ed0bb3e1ddcdd0d43
Lukas Appelhans <l.appelhans@gmx.de>
2013-07-31 20:40:17 +0200

    Add all needed files

---
+++ libmuon/backends/AkabeiBackend/AkabeiBackend.cpp
@@ -0,0 +1,132 @@
+/***************************************************************************
+ *   Copyright © 2013 Lukas Appelhans <boom1992@chakra-project.org>        *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU General Public License as        *
+ *   published by the Free Software Foundation; either version 2 of        *
+ *   the License or (at your option) version 3 or any later version        *
+ *   accepted by the membership of KDE e.V. (or its successor approved     *
+ *   by the membership of KDE e.V.), which shall act as a proxy            *
+ *   defined in Section 14 of version 3 of the license.                    *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ ***************************************************************************/
+#include "AkabeiBackend.h"
+#include "AkabeiResource.h"
+#include <akabeiclient/akabeiclientbackend.h>
+#include <akabeicore/akabeidatabase.h>
+#include <akabeicore/akabeiconfig.h>
+
+#include <KPluginFactory>
+#include <KLocalizedString>
+#include <KAboutData>
+#include <KDebug>
+
+K_PLUGIN_FACTORY(MuonAkabeiBackendFactory, registerPlugin<AkabeiBackend>(); )
+K_EXPORT_PLUGIN(MuonAkabeiBackendFactory(KAboutData("muon-akabeibackend","muon-akabeibackend",ki18n("Akabei Backend"),"0.1",ki18n("Chakra-Applications in your system"), KAboutData::License_GPL)))
+
+AkabeiBackend::AkabeiBackend(QObject* parent, const QVariantList& ) : AbstractResourcesBackend(parent)
+{
+    kDebug() << "CONSTRUCTED";
+    connect(AkabeiClient::Backend::instance(), SIGNAL(statusChanged(Akabei::Backend::Status)), SLOT(statusChanged(Akabei::Backend::Status)));
+    
+    /* Used to determine whether debugging prints are to be displayed later */
+    Akabei::Config::instance()->setDebug(true);
+
+    QLocale systemLocale = QLocale::system();
+    AkabeiClient::Backend::instance()->setLocale( systemLocale.name() );
+    AkabeiClient::Backend::instance()->initialize();
+}
+
+AkabeiBackend::~AkabeiBackend()
+{
+}
+
+void AkabeiBackend::statusChanged(Akabei::Backend::Status status)
+{
+    kDebug() << "Status changed to" << status;
+    if (status == Akabei::Backend::StatusReady && m_packages.isEmpty()) {
+        connect(Akabei::Backend::instance(), SIGNAL(queryPackagesCompleted(QUuid,QList<Akabei::Package*>)), SLOT(queryComplete(QUuid,QList<Akabei::Package*>)));
+        Akabei::Backend::instance()->packages();
+        
+    }
+    emit backendReady();
+}
+
+void AkabeiBackend::queryComplete(QUuid,QList<Akabei::Package*> packages)
+{
+    foreach (Akabei::Package * pkg, packages) {
+        m_packages.insert(pkg->name(), new AkabeiResource(pkg, this));
+    }
+}
+
+bool AkabeiBackend::isValid() const
+{
+    return true;
+}
+
+AbstractReviewsBackend* AkabeiBackend::reviewsBackend() const
+{
+    return nullptr;
+}
+
+AbstractResource* AkabeiBackend::resourceByPackageName(const QString& name) const
+{
+    return m_packages[name];
+}
+
+int AkabeiBackend::updatesCount() const
+{
+    return 0;
+}
+
+QVector< AbstractResource* > AkabeiBackend::allResources() const
+{
+    return m_packages.values().toVector();
+}
+
+QList< AbstractResource* > AkabeiBackend::searchPackageName(const QString& searchText)
+{
+    QList<AbstractResource*> result;
+    for (AbstractResource * res : m_packages.values()) {
+        if (res->name().contains(searchText, Qt::CaseInsensitive) || res->comment().contains(searchText, Qt::CaseInsensitive))
+            result << res;
+    }
+    return result;
+}
+    
+void AkabeiBackend::installApplication(AbstractResource* app, AddonList addons)
+{
+    installApplication(app);
+}
+
+void AkabeiBackend::installApplication(AbstractResource* app)
+{
+}
+
+void AkabeiBackend::removeApplication(AbstractResource* app)
+{
+
+}
+
+void AkabeiBackend::cancelTransaction(AbstractResource* app)
+{
+
+}
+
+AbstractBackendUpdater* AkabeiBackend::backendUpdater() const
+{
+    return nullptr;
+}
+
+QList< AbstractResource* > AkabeiBackend::upgradeablePackages() const
+{
+    return QList<AbstractResource*>();
+}
+
---
+++ libmuon/backends/AkabeiBackend/AkabeiResource.cpp
@@ -0,0 +1,151 @@
+/***************************************************************************
+ *   Copyright © 2013 Lukas Appelhans <boom1992@chakra-project.org>        *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU General Public License as        *
+ *   published by the Free Software Foundation; either version 2 of        *
+ *   the License or (at your option) version 3 or any later version        *
+ *   accepted by the membership of KDE e.V. (or its successor approved     *
+ *   by the membership of KDE e.V.), which shall act as a proxy            *
+ *   defined in Section 14 of version 3 of the license.                    *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ ***************************************************************************/
+#include "AkabeiResource.h"
+#include "AkabeiBackend.h"
+#include <QtCore/QStringList>
+#include <akabeicore/akabeidatabase.h>
+
+AkabeiResource::AkabeiResource(Akabei::Package * pkg, AkabeiBackend * parent)
+  : AbstractResource(parent),
+    m_pkg(pkg)
+{
+    if (pkg->isInstalled())
+        m_installedPkg = pkg;
+}
+        
+QString AkabeiResource::packageName() const
+{
+    return m_pkg->name();
+}
+        
+QString AkabeiResource::name()
+{
+    return m_pkg->name();
+}
+        
+QString AkabeiResource::comment()
+{
+    return m_pkg->description();
+}
+        
+QString AkabeiResource::icon() const
+{
+    return "akabei";
+}
+        
+bool AkabeiResource::canExecute() const
+{
+    return false;
+}
+        
+void AkabeiResource::invokeApplication() const
+{
+}
+        
+AbstractResource::State AkabeiResource::state()
+{
+    if (m_pkg->isInstalled())
+        return AbstractResource::Installed;
+    return AbstractResource::None;
+}
+        
+QString AkabeiResource::categories()
+{
+    return "Multimedia";
+}
+        
+QUrl AkabeiResource::homepage() const
+{
+    return m_pkg->url();
+}
+        
+bool AkabeiResource::isTechnical() const
+{
+    return false;
+}
+
+QUrl AkabeiResource::thumbnailUrl()
+{
+    return QUrl();
+}
+
+QUrl AkabeiResource::screenshotUrl()
+{
+    return QUrl();
+}
+        
+int AkabeiResource::downloadSize()
+{
+    return m_pkg->size();
+}
+
+QString AkabeiResource::license()
+{
+    return m_pkg->licenses().join(" ");
+}
+        
+QString AkabeiResource::installedVersion() const
+{
+    return m_installedPkg->version().toByteArray().data();
+}
+
+QString AkabeiResource::availableVersion() const
+{
+    return m_pkg->version().toByteArray().data();
+}
+
+QString AkabeiResource::longDescription() const
+{
+    return m_pkg->description();
+}
+        
+QString AkabeiResource::origin() const
+{
+    return m_pkg->database()->name();
+}
+
+QString AkabeiResource::section()
+{
+    return "multimedia";
+}
+        
+QString AkabeiResource::mimetypes() const
+{
+    return QString();
+}
+        
+QList<PackageState> AkabeiResource::addonsInformation()
+{
+    return QList<PackageState>();
+}
+
+bool AkabeiResource::isFromSecureOrigin() const
+{
+    return true;
+}
+        
+QStringList AkabeiResource::executables() const
+{
+    return QStringList();
+}
+
+void AkabeiResource::fetchScreenshots() {}
+void AkabeiResource::fetchChangelog() {}
+
