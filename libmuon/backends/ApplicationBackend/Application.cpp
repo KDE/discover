aba2095b414ee99e7c1d0860d8fc883099db673c
Lukas Appelhans <l.appelhans@gmx.de>
2013-10-10 16:03:37 +0200

    Revert "Remove the split again, don't know why it was added"
    This reverts commit 2937a8c18a5d9aa91d46769eeb851014c396b2ad.

--- libmuon/backends/ApplicationBackend/Application.cpp
+++ libmuon/backends/ApplicationBackend/Application.cpp
@@ -154,7 +154,7 @@ QString Application::icon() const
 
 QString Application::mimetypes() const
 {
-    return getField("MimeType");
+    return getField("MimeType").split(';');
 }
 
 QString Application::menuPath()
