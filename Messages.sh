4ed6293831044112019cffc633f020d3fbc775e2
Aleix Pol <aleixpol@kde.org>
2013-12-12 12:18:11 +0100

    Properly list all categories files
    From 2.0, we've had more categories than the ones from applications.

--- Messages.sh
+++ Messages.sh
@@ -4,7 +4,7 @@ $EXTRACTRC `find muon/ -name \*.rc` >> muonrc.cpp
 $EXTRACTRC `find updater/ -name \*.rc` >> updaterrc.cpp
 $EXTRACTRC `find kded/ -name \*.rc` >> notifierrc.cpp
 $EXTRACTRC `find libmuonapt/ -name \*.rc` >> libmuonapt.cpp
-$EXTRACTRC --context="Category" --tag-group=none --tag=Name `find libmuon -name apps-categories.xml` >> categoriesxml.cpp
+$EXTRACTRC --context="Category" --tag-group=none --tag=Name `find libmuon -name "*-categories.xml"` >> categoriesxml.cpp
 $XGETTEXT categoriesxml.cpp libmuonapt.cpp `find libmuon -name \*.cpp` `find libmuonapt -name \*.cpp` -o $podir/libmuon.pot
 $XGETTEXT rc.cpp muonrc.cpp `find muon -name \*.cpp` -o $podir/muon.pot
 $XGETTEXT rc.cpp updaterrc.cpp `find updater -name \*.cpp` -o $podir/muon-updater.pot
