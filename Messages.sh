5326151f7ef09aa356ea09089cc7816f98d06583
Aleix Pol <aleixpol@kde.org>
2015-01-14 18:30:32 +0100

    Remove muon-installer
    Muon Discover does the same things and it's more actively maintained.
    It wasn't ported to KF5.
    It was still muonapt-dependent.
    It has a bunch of bugs nobody really knows how to fix.
    
    CCMAIL: kubuntu-devel@lists.ubuntu.com

--- Messages.sh
+++ Messages.sh
@@ -6,7 +6,6 @@ $EXTRACTRC --context="Category" --tag-group=none --tag=Name `find libmuon -name 
 $XGETTEXT categoriesxml.cpp `find libmuon -name \*.cpp` `find libmuonapt -name \*.cpp` -o $podir/libmuon.pot
 $XGETTEXT rc.cpp muonrc.cpp `find muon -name \*.cpp` -o $podir/muon.pot
 $XGETTEXT rc.cpp updaterrc.cpp `find updater -name \*.cpp` -o $podir/muon-updater.pot
-$XGETTEXT rc.cpp `find installer -name \*.cpp` -o $podir/muon-installer.pot
 $XGETTEXT `find discover -name \*.cpp -o -name \*.qml` -o $podir/muon-discover.pot
 rm -f muonrc.cpp
 rm -f updaterrc.cpp
