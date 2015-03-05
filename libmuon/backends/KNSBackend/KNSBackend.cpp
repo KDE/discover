ab8cca09fb0dc7f18da37c2dd780d4778683f3bc
Aleix Pol <aleixpol@kde.org>
2015-03-05 18:30:28 +0100

    Add warning if an invalid category was found

--- libmuon/backends/KNSBackend/KNSBackend.cpp
+++ libmuon/backends/KNSBackend/KNSBackend.cpp
@@ -162,9 +162,10 @@ void KNSBackend::categoriesLoaded(Attica::BaseJob* job)
     // Otherwise we'll fetch an empty category specificier which can return
     // everything and the kitchen sink if the remote provider feels like it.
     for (auto it = m_categories.begin(); it != m_categories.end();) {
-        if (!it.value().isValid())
+        if (!it.value().isValid()) {
+            qWarning() << "Found invalid category" << it.key();
             it = m_categories.erase(it);
-        else
+        } else
             ++it;
     }
     if (m_categories.isEmpty()) {
