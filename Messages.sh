82c6700c1486cc0534a29bdcd57da0db5495ece8
Aleix Pol <aleixpol@kde.org>
2015-11-08 22:38:27 +0100

    Fix messages.sh
    Remove muon/muon

--- Messages.sh
+++ Messages.sh
@@ -1,10 +1,8 @@
 #! /usr/bin/env bash
 
-$EXTRACTRC `find muon/ -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> muonrc.cpp
 $EXTRACTRC `find updater/ -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> updaterrc.cpp
 $EXTRACTRC --context="Category" --tag-group=none --tag=Name `find libmuon -name "*-categories.xml"` >> categoriesxml.cpp
 $XGETTEXT categoriesxml.cpp `find libmuon -name \*.cpp` `find libmuonapt -name \*.cpp` -o $podir/libmuon.pot
-$XGETTEXT rc.cpp muonrc.cpp `find muon -name \*.cpp` -o $podir/muon.pot
 $XGETTEXT rc.cpp updaterrc.cpp `find updater -name \*.cpp` -o $podir/muon-updater.pot
 $XGETTEXT `find discover -name \*.cpp -o -name \*.qml -o -name \*.js` -o $podir/muon-discover.pot
 $XGETTEXT `find exporter -name \*.cpp` -o $podir/muon-exporter.pot
