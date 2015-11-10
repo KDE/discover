b96c976cf9cb5cc96cb94fec63124f069b548189
Aleix Pol <aleixpol@kde.org>
2015-11-10 19:24:16 +0100

    Adapt Messages.sh to internal renames

--- Messages.sh
+++ Messages.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 
 $EXTRACTRC `find updater/ -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> updaterrc.cpp
-$EXTRACTRC --context="Category" --tag-group=none --tag=Name `find libmuon -name "*-categories.xml"` >> categoriesxml.cpp
-$XGETTEXT categoriesxml.cpp `find libmuon -name \*.cpp` -o $podir/libmuon.pot
+$EXTRACTRC --context="Category" --tag-group=none --tag=Name `find libdiscover -name "*-categories.xml"` >> categoriesxml.cpp
+$XGETTEXT categoriesxml.cpp `find libdiscover -name \*.cpp` -o $podir/libdiscover.pot
 $XGETTEXT rc.cpp updaterrc.cpp `find updater -name \*.cpp` -o $podir/muon-updater.pot
 $XGETTEXT `find discover -name \*.cpp -o -name \*.qml -o -name \*.js` -o $podir/muon-discover.pot
 $XGETTEXT `find exporter -name \*.cpp` -o $podir/muon-exporter.pot
