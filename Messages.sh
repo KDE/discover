f037ef477b9b4bd99925db5bd296a381bf3ae59e
Aleix Pol <aleixpol@kde.org>
2016-04-04 13:30:49 +0200

    Remove Updater
    It's been unused for a while now, superseeded by Discover

--- Messages.sh
+++ Messages.sh
@@ -1,12 +1,9 @@
 #! /usr/bin/env bash
 
-$EXTRACTRC `find updater/ -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> updaterrc.cpp
 $EXTRACTRC --context="Category" --tag-group=none --tag=Name `find libdiscover -name "*-categories.xml"` >> categoriesxml.cpp
 $XGETTEXT categoriesxml.cpp `find libdiscover -name \*.cpp` -o $podir/libdiscover.pot
-$XGETTEXT rc.cpp updaterrc.cpp `find updater -name \*.cpp` -o $podir/plasma-discover-updater.pot
 $XGETTEXT `find discover -name \*.cpp -o -name \*.qml -o -name \*.js` -o $podir/plasma-discover.pot
 $XGETTEXT `find exporter -name \*.cpp` -o $podir/plasma-discover-exporter.pot
 $XGETTEXT `find notifier -name \*.cpp` -o $podir/plasma-discover-notifier.pot
 rm -f muonrc.cpp
-rm -f updaterrc.cpp
 rm -f categoriesxml.cpp
