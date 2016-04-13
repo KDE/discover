ed9cb48f9d4a61ac645d6a5270739769d979e8cb
Aleix Pol <aleixpol@kde.org>
2016-04-13 15:53:27 +0200

    Assume exporter is not fit to install for now
    BUG: 361712

--- Messages.sh
+++ Messages.sh
@@ -3,7 +3,6 @@
 $EXTRACTRC --context="Category" --tag-group=none --tag=Name `find libdiscover -name "*-categories.xml"` >> categoriesxml.cpp
 $XGETTEXT categoriesxml.cpp `find libdiscover -name \*.cpp` -o $podir/libdiscover.pot
 $XGETTEXT `find discover -name \*.cpp -o -name \*.qml -o -name \*.js` -o $podir/plasma-discover.pot
-$XGETTEXT `find exporter -name \*.cpp` -o $podir/plasma-discover-exporter.pot
 $XGETTEXT `find notifier -name \*.cpp` -o $podir/plasma-discover-notifier.pot
 rm -f muonrc.cpp
 rm -f categoriesxml.cpp
