663b245051ad49fd6b98e79de0dd3ae0cafc154f
Bhushan Shah <bhush94@gmail.com>
2016-08-26 16:38:46 +0530

    Set proper component name for plasma-discover
    Summary:
    Plasma discover installs desktop file as org.kde.discover, but it
    instead sets its component name as muondiscover, and breaks the RDN
    scheme.
    
    this fixes window icon for discover
    
    Reviewers: #plasma, graesslin, apol
    
    Reviewed By: apol
    
    Subscribers: plasma-devel
    
    Tags: #plasma
    
    Differential Revision: https://phabricator.kde.org/D2587

--- discover/main.cpp
+++ discover/main.cpp
@@ -95,7 +95,7 @@ int main(int argc, char** argv)
     KCrash::initialize();
 #endif
     KLocalizedString::setApplicationDomain("plasma-discover");
-    KAboutData about(QStringLiteral("muondiscover"), i18n("Discover"), version, i18n("An application explorer"),
+    KAboutData about(QStringLiteral("discover"), i18n("Discover"), version, i18n("An application explorer"),
                      KAboutLicense::GPL, i18n("© 2010-2016 Plasma Development Team"));
     about.addAuthor(i18n("Aleix Pol Gonzalez"), QString(), QStringLiteral("aleixpol@blue-systems.com"));
     about.addAuthor(i18n("Jonathan Thomas"), QString(), QStringLiteral("echidnaman@kubuntu.org"));
