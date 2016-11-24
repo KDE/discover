369ce7b623008753e8e65c7378fdc4be1fa870bc
Aleix Pol <aleixpol@kde.org>
2016-11-24 16:36:28 +0100

    AppStream: Take stock icons into account

--- libdiscover/backends/PackageKitBackend/AppPackageKitResource.cpp
+++ libdiscover/backends/PackageKitBackend/AppPackageKitResource.cpp
@@ -63,6 +63,7 @@ QVariant AppPackageKitResource::icon() const
     if (icons.isEmpty()) {
         ret = QIcon::fromTheme(QStringLiteral("package-x-generic"));
     } else foreach(const AppStream::Icon &icon, icons) {
+        QStringList stock;
         switch(icon.kind()) {
             case AppStream::Icon::KindLocal:
                 ret.addFile(icon.url().toLocalFile(), icon.size());
@@ -70,8 +71,14 @@ QVariant AppPackageKitResource::icon() const
             case AppStream::Icon::KindCached:
                 ret.addFile(icon.url().toLocalFile(), icon.size());
                 break;
+            case AppStream::Icon::KindStock:
+                stock += icon.name();
+                break;
             default:
                 break;
+        }
+        if (ret.isNull() && !stock.isEmpty()) {
+            ret = QIcon::fromTheme(stock.first());
         }
     }
 #else
