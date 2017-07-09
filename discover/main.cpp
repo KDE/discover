3114021e572e63004bce2cc2e453d93b40f68ce8
David Faure <faure@kde.org>
2017-07-09 10:35:43 +0200

    Call QCLP parse rather than process, as documented in KDBusService.
    Doesn't change behaviour in practice here, because process is already
    called in all cases at the beginning of the method (=> double parsing
    of arguments, a bit unusual)

--- discover/main.cpp
+++ discover/main.cpp
@@ -126,7 +126,7 @@ int main(int argc, char** argv)
             if (arguments.isEmpty())
                 return;
             QScopedPointer<QCommandLineParser> parser(createParser());
-            parser->process(arguments);
+            parser->parse(arguments);
             processArgs(parser.data(), mainWindow);
         });
 
