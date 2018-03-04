da6be2affd9d878037314516c4875d5242587aa8
Nathaniel Graham <nate@kde.org>
2018-03-03 19:39:35 -0700

    Improve "Package removal" dialog wording
    Summary:
    Improve the wording of the "package removal" dialog to feel a bit more natural and introduce less uncertainty.
    
    The list of packages should also be alphabetically sorted, but I couldn't figure out how to do that right now and will do it in a follow-up patch.
    
    Test Plan:
    Before:
    {F5738085}
    
    After:
    {F5738087}
    
    Reviewers: #discover_software_store, apol
    
    Reviewed By: #discover_software_store, apol
    
    Subscribers: plasma-devel
    
    Tags: #plasma
    
    Differential Revision: https://phabricator.kde.org/D10992

--- libdiscover/backends/PackageKitBackend/PKTransaction.cpp
+++ libdiscover/backends/PackageKitBackend/PKTransaction.cpp
@@ -164,7 +164,7 @@ void PKTransaction::cleanup(PackageKit::Transaction::Exit exit, uint runtime)
             }
             msg += QStringLiteral("</li></ul>");
 
-            Q_EMIT proceedRequest(i18n("Confirm..."), i18np("To proceed with this action, the following package needs removal:\n%2", "To proceed with this action, the following packages need removal:\n%2", packagesToRemove.count(), msg));
+            Q_EMIT proceedRequest(i18n("Confirm package removal"), i18np("This action will also remove the following package:\n%2", "This action will also remove the following packages:\n%2", packagesToRemove.count(), msg));
         } else {
             proceed();
         }
