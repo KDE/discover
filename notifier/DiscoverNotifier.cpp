f237e7c0ccc87d4c9ebc49b68736e98a5012e1f4
Kai Uwe Broulik <kde@privat.broulik.de>
2018-10-15 09:59:06 +0200

    [Discover Notifier] Open Updates page in notification
    When clicking the "there are new updates" notification it should go to the Updates page rather than just opening Discover
    
    Differential Revision: https://phabricator.kde.org/D16152

--- notifier/DiscoverNotifier.cpp
+++ notifier/DiscoverNotifier.cpp
@@ -84,7 +84,7 @@ void DiscoverNotifier::showUpdatesNotification()
     const QString name = i18n("Update");
     e->setDefaultAction(name);
     e->setActions({name});
-    connect(e, QOverload<unsigned int>::of(&KNotification::activated), this, &DiscoverNotifier::showDiscover);
+    connect(e, QOverload<unsigned int>::of(&KNotification::activated), this, &DiscoverNotifier::showDiscoverUpdates);
 }
 
 void DiscoverNotifier::updateStatusNotifier()
