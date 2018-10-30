892095df5d44f4adaea5bf29f2f52284bc06041a
Aleix Pol <aleixpol@kde.org>
2018-10-30 18:10:59 +0100

    flatpak: use the path to identify a local remote

--- libdiscover/backends/FlatpakBackend/FlatpakSourcesBackend.cpp
+++ libdiscover/backends/FlatpakBackend/FlatpakSourcesBackend.cpp
@@ -243,7 +243,7 @@ void FlatpakSourcesBackend::addRemote(FlatpakRemote *remote, FlatpakInstallation
 
     FlatpakSourceItem *it = new FlatpakSourceItem(!title.isEmpty() ? title : id);
     it->setCheckState(flatpak_remote_get_disabled(remote) ? Qt::Unchecked : Qt::Checked);
-    it->setData(remoteUrl.host(), Qt::ToolTipRole);
+    it->setData(remoteUrl.isLocalFile() ? remoteUrl.toLocalFile() : remoteUrl.host(), Qt::ToolTipRole);
     it->setData(id, IdRole);
     it->setFlatpakInstallation(installation);
 
