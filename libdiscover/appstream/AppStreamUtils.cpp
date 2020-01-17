744061500563fcf22db7ad3da47ac69fa5ac3fed
Aleix Pol <aleixpol@kde.org>
2020-01-17 16:11:07 +0100

    fix build for older Qt to 5.14.0

--- libdiscover/appstream/AppStreamUtils.cpp
+++ libdiscover/appstream/AppStreamUtils.cpp
@@ -111,7 +111,7 @@ QStringList AppStreamUtils::appstreamIds(const QUrl &appstreamUrl)
     ret += appstreamUrl.host().isEmpty() ? appstreamUrl.path() : appstreamUrl.host();
     if (appstreamUrl.hasQuery()) {
         QUrlQuery query(appstreamUrl);
-        ret << query.queryItemValue(QStringLiteral("alt")).split(QLatin1Char(','), Qt::SkipEmptyParts);
+        ret << query.queryItemValue(QStringLiteral("alt")).split(QLatin1Char(','), QString::SkipEmptyParts);
     }
     return ret;
 }
