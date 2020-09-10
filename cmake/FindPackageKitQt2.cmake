a3f897ac1ca8f0a2eceb929424f6b94b678eac8a
David Redondo <kde@david-redondo.de>
2020-09-10 20:15:06 +0200

    Convert copyright statements to SPDX expressions

--- cmake/FindPackageKitQt2.cmake
+++ cmake/FindPackageKitQt2.cmake
@@ -5,33 +5,11 @@
 #  PACKAGEKITQT2_INCLUDEDIR - the PackageKitQt2 include directory
 #  PACKAGEKITQT2_LIBRARY - Link this to use the PackageKitQt2
 #
-# Copyright © 2010, Mehrdad Momeny <mehrdad.momeny@gmail.com>
-# Copyright © 2010, Harald Sitter <apachelogger@ubuntu.com>
-# Copyright © 2013, Lukas Appelhans <l.appelhans@gmx.de>
+# SPDX-FileCopyrightText: 2010 Mehrdad Momeny <mehrdad.momeny@gmail.com>
+# SPDX-FileCopyrightText: 2010 Harald Sitter <apachelogger@ubuntu.com>
+# SPDX-FileCopyrightText: 2013 Lukas Appelhans <l.appelhans@gmx.de>
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. Neither the name of the University nor the names of its contributors
-#    may be used to endorse or promote products derived from this software
-#    without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
-# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
-# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
-# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
-# SUCH DAMAGE.
+# SPDX-License-Identifier: BSD-3-Clause
 
 if (NOT WIN32)
     find_package(PkgConfig)
