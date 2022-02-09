eb81046825606fa6ed7973dc3b5eb3413c6a355c
Fabian Vogt <fabian@ritter-vogt.de>
2022-02-09 22:28:56 +0100

    notifier: Actually save LastUnattendedTrigger
    Unlike QSettings, saving does not happen automatically.
    
    (cherry picked from commit fd9a9b71116730beb2708557492541832d01230d)

--- notifier/UnattendedUpdates.cpp
+++ notifier/UnattendedUpdates.cpp
@@ -77,6 +77,7 @@ void UnattendedUpdates::triggerUpdate(int timeoutId)
         notifier->setBusy(false);
         process->deleteLater();
         notifier->settings()->setLastUnattendedTrigger(QDateTime::currentDateTimeUtc());
+        notifier->settings()->save();
     });
 
     notifier->setBusy(true);
