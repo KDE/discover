327f8ec98ae37697b1b0c4388c4a27244012e8c9
Fushan Wen <qydwhotmail@gmail.com>
2022-12-18 10:52:31 +0800

    libdiscover: fix permission text not being translated
    Add qml files to gettext
    
    
    (cherry picked from commit ac7769a744a527ed30cfcc149ae2c571838f5bfb)

--- Messages.sh
+++ Messages.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 $EXTRACTRC --context="Category" --tag-group=none --tag=Name `find libdiscover -name "*-categories.xml"` >> rc.cpp
-$XGETTEXT rc.cpp `find libdiscover -name \*.cpp` -o $podir/libdiscover.pot
+$XGETTEXT rc.cpp `find libdiscover -name \*.cpp -o -name \*.qml` -o $podir/libdiscover.pot
 $XGETTEXT `find discover -name \*.cpp -o -name \*.qml -o -name \*.js` -o $podir/plasma-discover.pot
 $XGETTEXT `find notifier -name \*.cpp` -o $podir/plasma-discover-notifier.pot
 $XGETTEXT `find kcm -name \*.cpp -o -name \*.qml` -o $podir/kcm_updates.pot
