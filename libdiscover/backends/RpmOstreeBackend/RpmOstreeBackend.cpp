1756b3a8ac7b66fd1c93a823e6dde262d1f2a9ec
Timothée Ravier <tim@siosm.fr>
2023-04-17 20:56:31 +0200

    rpm-ostree: Do not offer resources when the search doesn't pertain
    Added Category include to fix build.
    
    BUG: 468162
    Co-authored with: Aleix Pol <aleixpol@kde.org>
    Cherry-picked from: 7bd3ba098ca97a01ba01f9d0fadd6522136aa67e

--- libdiscover/backends/RpmOstreeBackend/RpmOstreeBackend.cpp
+++ libdiscover/backends/RpmOstreeBackend/RpmOstreeBackend.cpp
@@ -12,6 +12,7 @@
 
 #include <AppStreamQt/release.h>
 #include <AppStreamQt/utils.h>
+#include <Category/Category.h>
 #include <KLocalizedString>
 #include <appstream/AppStreamIntegration.h>
 
@@ -474,6 +475,14 @@ bool RpmOstreeBackend::isValid() const
 
 ResultsStream *RpmOstreeBackend::search(const AbstractResourcesBackend::Filters &filter)
 {
+    // Only return results when we're explicitely looking in the "Operating System" category
+    if (!filter.category) {
+        return new ResultsStream(QStringLiteral("rpm-ostree-empty"), {});
+    }
+    if (filter.category->untranslatedName() != QLatin1String("Operating System")) {
+        return new ResultsStream(QStringLiteral("rpm-ostree-empty"), {});
+    }
+
     QVector<AbstractResource *> res;
     for (RpmOstreeResource *r : m_resources) {
         if (r->state() >= filter.state) {
