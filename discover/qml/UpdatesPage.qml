KDE/discover

commit 1c78feaa7be0bb94c552d95dd4a71e84d3923006

Add feature to install offline updates and shut down, rather than just restarting after
This allows a user to shut down their computer after offline updates have been completed, without the computer restarting again on update completion.

discover/qml/UpdatesPage.qml
@@ -236,10 +236,29 @@ DiscoverPage {
                     onClicked: { updateModel.uncheckAll(); }
                 }
 
-                QQC2.CheckBox {
-                    id: rebootAtEnd
+                RowLayout {
                     visible: resourcesUpdatesModel.needsReboot && resourcesUpdatesModel.isProgressing
-                    text: i18n("Restart automatically after update has completed");
+                    spacing: Kirigami.units.smallSpacing
+
+                    Layout.fillWidth: true
+
+                    Layout.leftMargin: Kirigami.Units.largeSpacing
+                    Layout.rightMargin: Kirigami.Units.largeSpacing
+
+                    RowLayout {
+                        visible: resourcesUpdatesModel.needsReboot && resourcesUpdatesModel.isProgressing
+                        spacing: Kirigami.units.smallSpacing
+                        Layout.fillWidth: true
+
+                        QQC2.Label {
+                            text: i18nc("@info on the completion of updates, the action that automatically happens after (e.g shutdown)", "On completion, automatically:")
+                        }
+
+                        QQC2.ComboBox {
+                            id: actionAfterUpdateCombo
+                            model: [i18nc("@item:inlistbox placeholder for when no action is selected", "Select an action"), i18nc("@item:inlistbox", "Restart"), i18nc("@item:inlistbox", "Shutdown")]
+                        }
+                    }
                 }
 
                 QQC2.Label {
@@ -279,12 +298,13 @@ DiscoverPage {
         }
 
         Kirigami.Action {
-            id: restartAction
-            icon.name: "system-reboot"
-            text: i18n("Restart Now")
+            id: promptRestartAction
+            icon.name: "system-reboot-update"
+            text: i18nc("@action:button", "Restart to Install Updates Now")
             visible: false
-            onTriggered: app.reboot()
+            onTriggered: app.promptReboot()
         }
+
         Kirigami.LoadingPlaceholder {
             id: statusLabel
             icon.name: {
@@ -434,7 +454,7 @@ DiscoverPage {
                     }
                 }
 
-                RowLayout {
+                ColumnLayout {
                     Layout.fillWidth: true
                     spacing: Kirigami.Units.smallSpacing
                     visible: listItem.model.extended
@@ -511,12 +531,16 @@ DiscoverPage {
         State {
             name: "reboot"
             PropertyChanges { target: page; footerLabel: i18nc("@info", "Restart the system to complete the update process") }
-            PropertyChanges { target: statusLabel; helpfulAction: restartAction }
+            PropertyChanges { target: statusLabel; helpfulAction: promptRestartAction }
             PropertyChanges { target: statusLabel; explanation: "" }
             PropertyChanges { target: statusLabel.progressBar; visible: false }
             StateChangeScript {
-                script: if (rebootAtEnd.checked && resourcesUpdatesModel.readyToReboot) {
-                    app.rebootNow()
+                script: if (resourcesUpdatesModel.readyToReboot) {
+                    if (actionAfterUpdateCombo.currentIndex === 1) {
+                        app.rebootNow()
+                    } else if (actionAfterUpdateCombo.currentIndex === 2) {
+                        app.shutdownNow()
+                    }
                 }
             }
         },

discover/DiscoverObject.cpp
@@ -20,6 +20,7 @@
 #include <QDBusConnection>
 #include <QDBusMessage>
 #include <QDBusPendingCall>
+#include <QDBusPendingReply>
 #include <QDesktopServices>
 #include <QFile>
 #include <QGuiApplication>
@@ -641,21 +642,55 @@ void DiscoverObject::copyTextToClipboard(const QString text)
     clipboard->setText(text);
 }
 
-void DiscoverObject::reboot()
+void DiscoverObject::setAboutToPowerOff()
+{
+    for (auto backend : ResourcesModel::global()->backends()) {
+        backend->aboutTo(AbstractResourcesBackend::PowerOff);
+    }
+}
+
+void DiscoverObject::setAboutToReboot()
+{
+    for (auto backend : ResourcesModel::global()->backends()) {
+        backend->aboutTo(AbstractResourcesBackend::Reboot);
+    }
+}
+
+void DiscoverObject::promptReboot()
 {
     auto method = QDBusMessage::createMethodCall(QStringLiteral("org.kde.LogoutPrompt"),
                                                  QStringLiteral("/LogoutPrompt"),
                                                  QStringLiteral("org.kde.LogoutPrompt"),
-                                                 QStringLiteral("promptReboot"));
+                                                 QStringLiteral("promptAll"));
     QDBusConnection::sessionBus().asyncCall(method);
 }
 
 void DiscoverObject::rebootNow()
 {
+    setAboutToReboot();
     auto method = QDBusMessage::createMethodCall(QStringLiteral("org.freedesktop.login1"),
                                                  QStringLiteral("/org/freedesktop/login1"),
                                                  QStringLiteral("org.freedesktop.login1.Manager"),
                                                  QStringLiteral("Reboot"));
+    method.setArguments({true /*interactive*/});
+    QDBusConnection::systemBus().asyncCall(method);
+}
+
+void DiscoverObject::shutdownNow()
+{
+    bool shouldRebootFirst = false;
+    for (auto backend : ResourcesModel::global()->backends()) {
+        if (backend->needsRebootForPowerOffAction()) {
+            shouldRebootFirst = true;
+            break;
+        }
+    }
+    setAboutToPowerOff();
+
+    auto method = QDBusMessage::createMethodCall(QStringLiteral("org.freedesktop.login1"),
+                                                 QStringLiteral("/org/freedesktop/login1"),
+                                                 QStringLiteral("org.freedesktop.login1.Manager"),
+                                                 shouldRebootFirst ? QStringLiteral("Reboot") : QStringLiteral("PowerOff"));
     method.setArguments({true /*interactive*/});
     QDBusConnection::systemBus().asyncCall(method);
 }

discover/DiscoverObject.h
@@ -58,8 +58,9 @@ public Q_SLOTS:
     void openMode(const QString &mode);
     void openLocalPackage(const QUrl &localfile);
 
-    void reboot();
+    void promptReboot();
     void rebootNow();
+    void shutdownNow();
 
 private Q_SLOTS:
     void switchApplicationLanguage();
@@ -80,6 +81,8 @@ private:
     void initMainWindow(QQuickWindow *mainWindow);
     bool quitWhenIdle();
     void reconsiderQuit();
+    void setAboutToReboot();
+    void setAboutToPowerOff();
     QQmlApplicationEngine *engine() const
     {
         return m_engine;

libdiscover/backends/PackageKitBackend/PackageKitBackend.cpp
@@ -1198,5 +1198,19 @@ void PackageKitBackend::loadAllPackages()
     m_allPackagesLoaded = true;
 }
 
+void PackageKitBackend::aboutTo(AboutToAction action)
+{
+    PackageKit::Offline::Action packageKitAction;
+    switch (action) {
+    case Reboot:
+        packageKitAction = PackageKit::Offline::ActionReboot;
+        break;
+    case PowerOff:
+        packageKitAction = PackageKit::Offline::ActionPowerOff;
+        break;
+    }
+    m_updater->setOfflineUpdateAction(packageKitAction);
+}
+
 #include "PackageKitBackend.moc"
 #include "moc_PackageKitBackend.cpp"

libdiscover/backends/PackageKitBackend/PackageKitBackend.h
@@ -7,7 +7,9 @@
 #pragma once
 
 #include "PackageKitResource.h"
+
 #include <AppStreamQt/pool.h>
+#include <PackageKit/Offline>
 #include <PackageKit/Transaction>
 #include <QFile>
 #include <QPointer>
@@ -129,6 +131,11 @@ public:
     QStringList globalHints()
     {
         return m_globalHints;
+    }
+    void aboutTo(AboutToAction action) override;
+    bool needsRebootForPowerOffAction() const override
+    {
+        return true;
     }
 
 public Q_SLOTS:

libdiscover/backends/PackageKitBackend/PackageKitUpdater.cpp
@@ -633,7 +633,7 @@ void PackageKitUpdater::finished(PackageKit::Transaction::Exit exit, uint /*time
             m_backend->clear();
             setProgressing(false);
         } else {
-            PackageKit::Daemon::global()->offline()->trigger(PackageKit::Offline::ActionReboot);
+            PackageKit::Daemon::global()->offline()->trigger(m_offlineUpdateAction);
         }
         enableReadyToReboot();
     }
@@ -914,6 +914,14 @@ double PackageKitUpdater::updateSize() const
 quint64 PackageKitUpdater::downloadSpeed() const
 {
     return m_transaction ? m_transaction->speed() : 0;
+}
+
+void PackageKitUpdater::setOfflineUpdateAction(PackageKit::Offline::Action action)
+{
+    m_offlineUpdateAction = action;
+    if (PackageKit::Daemon::global()->offline()->updateTriggered()) {
+        PackageKit::Daemon::global()->offline()->trigger(action);
+    }
 }
 
 #include "PackageKitUpdater.moc"

libdiscover/backends/PackageKitBackend/PackageKitUpdater.h
@@ -49,6 +49,7 @@ public:
     void setDistroUpgrade(const AppStream::Release &release);
     void clearDistroUpgrade();
     bool isDistroUpgrade() const;
+    void setOfflineUpdateAction(PackageKit::Offline::Action action);
 
 public Q_SLOTS:
     /// must be implemented if ever isCancelable is true
@@ -106,6 +107,7 @@ private:
     QDateTime m_lastUpdate;
     QMap<PackageKit::Transaction::Info, QStringList> m_packagesModified;
     QVector<std::function<PackageKit::Transaction *()>> m_proceedFunctions;
+    PackageKit::Offline::Action m_offlineUpdateAction = PackageKit::Offline::Action::ActionReboot;
 
     SystemUpgrade *m_upgrade = nullptr;
 };

libdiscover/resources/AbstractResourcesBackend.h
@@ -239,6 +239,23 @@ public:
      */
     virtual uint fetchingUpdatesProgressWeight() const;
 
+    enum AboutToAction { Reboot, PowerOff };
+
+    /**
+     * Notifies the backend to prepare for a certain power action.
+     * Currently only used in the PackageKit backend.
+     * @param action the action that will occur
+     */
+    virtual void aboutTo(AboutToAction action) {};
+
+    /**
+     * @returns if the backend needs a reboot even when the PowerOff action is chosen,
+     * which would occur if the PowerOff happens after a reboot happens first
+     */
+    virtual bool needsRebootForPowerOffAction() const
+    {
+        return false;
+    }
 public Q_SLOTS:
     /**
      * This gets called when the backend should install an application.

commit bf24142dce34edfbad41d3d001aabbd4768d46c4

UpdatesPage: return to using a RowLayout in expanded delegate view
Accidentally regressed in 1c78feaa7be0bb94c552d95dd4a71e84d3923006.

discover/qml/UpdatesPage.qml
@@ -454,7 +454,7 @@ DiscoverPage {
                     }
                 }
 
-                ColumnLayout {
+                RowLayout {
                     Layout.fillWidth: true
                     spacing: Kirigami.Units.smallSpacing
                     visible: listItem.model.extended