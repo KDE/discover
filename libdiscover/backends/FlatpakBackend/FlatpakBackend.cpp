4a045f1550d90a033325f0e21003e9d8aa06a67d
Aleix Pol <aleixpol@kde.org>
2024-08-31 01:18:42 +0200

    Revert "flatpak: Do not check both app and runtime, use the information we know"
    Improved the explanation so I'm not tempted to remove the code again
    
    This reverts commit f756f67ac79ac6b9b6d80e7f478fb0bd607d0069.

--- libdiscover/backends/FlatpakBackend/FlatpakBackend.cpp
+++ libdiscover/backends/FlatpakBackend/FlatpakBackend.cpp
@@ -1579,13 +1579,24 @@ ResultsStream *FlatpakBackend::search(const AbstractResourcesBackend::Filters &f
 
                             const QByteArray parentId(id.constData(), id.lastIndexOf(QLatin1Char('.')));
 
+                            // We need to bruteforce the API as we don't know from here if ref's parent is an app or a runtime 🙄
                             auto parentRef = flatpak_installation_get_installed_ref(installation,
-                                                                                    flatpak_ref_get_kind(FLATPAK_REF(ref)),
+                                                                                    FLATPAK_REF_KIND_APP,
                                                                                     parentId.constData(),
                                                                                     flatpak_ref_get_arch(FLATPAK_REF(ref)),
                                                                                     flatpak_ref_get_branch(FLATPAK_REF(ref)),
                                                                                     cancellable,
                                                                                     &localError);
+                            if (!parentRef) {
+                                g_clear_error(&localError);
+                                parentRef = flatpak_installation_get_installed_ref(installation,
+                                                                                   FLATPAK_REF_KIND_RUNTIME,
+                                                                                   parentId.constData(),
+                                                                                   flatpak_ref_get_arch(FLATPAK_REF(ref)),
+                                                                                   flatpak_ref_get_branch(FLATPAK_REF(ref)),
+                                                                                   cancellable,
+                                                                                   &localError);
+                            }
                             if (parentRef) {
                                 if (auto resource = self->getAppForInstalledRef(installation, parentRef)) {
                                     resource->addRefToUpdate(flatpak_ref_format_ref_cached(FLATPAK_REF(parentRef)));
