a2cc8212f60949b9c9be48f13819da27b8e4c667
Fushan Wen <qydwhotmail@gmail.com>
2025-01-19 17:12:57 +0800

    libdiscover: always emit reviewsReady to fix crash when there is no review
    OdrsReviewsBackend needs that signal to remove OdrsReviewsJob from
    m_jobs, otherwise OdrsReviewsBackend::fetchReviews will return an
    invalid job pointer after OdrsReviewsJob calls deleteLater().

--- libdiscover/appstream/OdrsReviewsJob.cpp
+++ libdiscover/appstream/OdrsReviewsJob.cpp
@@ -82,80 +82,78 @@ void OdrsReviewsJob::reviewsFetched()
 void OdrsReviewsJob::parseReviews(const QJsonDocument &document)
 {
     const auto reviews = document.array();
-    if (!reviews.isEmpty()) {
-        QList<ReviewPtr> reviewsList;
-        for (const auto &it : reviews) {
-            const QJsonObject review = it.toObject();
-            if (!review.isEmpty()) {
-                // Same ranking algorythm Gnome Software uses
-                const int usefulFavorable = review.value(QLatin1StringView("karma_up")).toInt();
-                const int usefulNegative = review.value(QLatin1StringView("karma_down")).toInt();
-                const int usefulTotal = usefulFavorable + usefulNegative;
+    QList<ReviewPtr> reviewsList;
+    for (const auto &it : reviews) {
+        const QJsonObject review = it.toObject();
+        if (!review.isEmpty()) {
+            // Same ranking algorythm Gnome Software uses
+            const int usefulFavorable = review.value(QLatin1StringView("karma_up")).toInt();
+            const int usefulNegative = review.value(QLatin1StringView("karma_down")).toInt();
+            const int usefulTotal = usefulFavorable + usefulNegative;
 
-                qreal usefulWilson = 0.0;
+            qreal usefulWilson = 0.0;
 
-                /* from http://www.evanmiller.org/how-not-to-sort-by-average-rating.html */
-                if (usefulFavorable > 0 || usefulNegative > 0) {
-                    usefulWilson = ((usefulFavorable + 1.9208) / (usefulFavorable + usefulNegative)
-                                    - 1.96 * sqrt((usefulFavorable * usefulNegative) / qreal(usefulFavorable + usefulNegative) + 0.9604)
-                                        / (usefulFavorable + usefulNegative))
-                        / (1 + 3.8416 / (usefulFavorable + usefulNegative));
-                    usefulWilson *= 100.0;
-                }
-
-                QDateTime dateTime;
-                dateTime.setSecsSinceEpoch(review.value(QLatin1StringView("date_created")).toInt());
-
-                // If there is no score or the score is the same, base on the age
-                const auto currentDateTime = QDateTime::currentDateTime();
-                const auto totalDays = static_cast<qreal>(dateTime.daysTo(currentDateTime));
-
-                // use also the longest common subsequence of the version string to compute relevance
-                const auto reviewVersion = review.value(QLatin1StringView("version")).toString();
-                const auto availableVersion = m_resource->availableVersion();
-                qreal versionScore = 0;
-                const int minLength = std::min(reviewVersion.length(), availableVersion.length());
-                if (minLength > 0) {
-                    for (int i = 0; i < minLength; ++i) {
-                        if (reviewVersion[i] != availableVersion[i] || i == minLength - 1) {
-                            versionScore = i;
-                            break;
-                        }
-                    }
-                    // Normalize
-                    versionScore = versionScore / qreal(std::max(reviewVersion.length(), availableVersion.length()) - 1);
-                }
-
-                // Very random heuristic which weights usefulness with age and version similarity. Don't penalize usefulness more than 6 months
-                usefulWilson = versionScore + 1.0 / std::max(1.0, totalDays) + usefulWilson / std::clamp(totalDays, 1.0, 93.0);
-
-                const bool shouldShow = usefulFavorable >= usefulNegative * 2 && review.value(QLatin1StringView("reported")).toInt() < 4;
-                ReviewPtr r(new Review(review.value(QLatin1StringView("app_id")).toString(),
-                                       m_resource->packageName(),
-                                       review.value(QLatin1StringView("locale")).toString(),
-                                       review.value(QLatin1StringView("summary")).toString(),
-                                       review.value(QLatin1StringView("description")).toString(),
-                                       review.value(QLatin1StringView("user_display")).toString(),
-                                       dateTime,
-                                       shouldShow,
-                                       review.value(QLatin1StringView("review_id")).toInt(),
-                                       review.value(QLatin1StringView("rating")).toInt() / 10,
-                                       usefulTotal,
-                                       usefulFavorable,
-                                       usefulWilson,
-                                       reviewVersion));
-                // We can also receive just a json with app name and user info so filter these out as there is no review
-                if (!r->summary().isEmpty() && !r->reviewText().isEmpty()) {
-                    reviewsList.append(r);
-                    // Needed for submitting usefulness
-                    r->addMetadata(QLatin1StringView("ODRS::user_skey"), review.value(QLatin1StringView("user_skey")).toString());
-                }
-
-                // We should get at least user_skey needed for posting reviews
-                m_resource->addMetadata(QLatin1StringView("ODRS::user_skey"), review.value(QLatin1StringView("user_skey")).toString());
+            /* from http://www.evanmiller.org/how-not-to-sort-by-average-rating.html */
+            if (usefulFavorable > 0 || usefulNegative > 0) {
+                usefulWilson =
+                    ((usefulFavorable + 1.9208) / (usefulFavorable + usefulNegative)
+                     - 1.96 * sqrt((usefulFavorable * usefulNegative) / qreal(usefulFavorable + usefulNegative) + 0.9604) / (usefulFavorable + usefulNegative))
+                    / (1 + 3.8416 / (usefulFavorable + usefulNegative));
+                usefulWilson *= 100.0;
             }
-        }
 
-        Q_EMIT reviewsReady(reviewsList, false);
+            QDateTime dateTime;
+            dateTime.setSecsSinceEpoch(review.value(QLatin1StringView("date_created")).toInt());
+
+            // If there is no score or the score is the same, base on the age
+            const auto currentDateTime = QDateTime::currentDateTime();
+            const auto totalDays = static_cast<qreal>(dateTime.daysTo(currentDateTime));
+
+            // use also the longest common subsequence of the version string to compute relevance
+            const auto reviewVersion = review.value(QLatin1StringView("version")).toString();
+            const auto availableVersion = m_resource->availableVersion();
+            qreal versionScore = 0;
+            const int minLength = std::min(reviewVersion.length(), availableVersion.length());
+            if (minLength > 0) {
+                for (int i = 0; i < minLength; ++i) {
+                    if (reviewVersion[i] != availableVersion[i] || i == minLength - 1) {
+                        versionScore = i;
+                        break;
+                    }
+                }
+                // Normalize
+                versionScore = versionScore / qreal(std::max(reviewVersion.length(), availableVersion.length()) - 1);
+            }
+
+            // Very random heuristic which weights usefulness with age and version similarity. Don't penalize usefulness more than 6 months
+            usefulWilson = versionScore + 1.0 / std::max(1.0, totalDays) + usefulWilson / std::clamp(totalDays, 1.0, 93.0);
+
+            const bool shouldShow = usefulFavorable >= usefulNegative * 2 && review.value(QLatin1StringView("reported")).toInt() < 4;
+            ReviewPtr r(new Review(review.value(QLatin1StringView("app_id")).toString(),
+                                   m_resource->packageName(),
+                                   review.value(QLatin1StringView("locale")).toString(),
+                                   review.value(QLatin1StringView("summary")).toString(),
+                                   review.value(QLatin1StringView("description")).toString(),
+                                   review.value(QLatin1StringView("user_display")).toString(),
+                                   dateTime,
+                                   shouldShow,
+                                   review.value(QLatin1StringView("review_id")).toInt(),
+                                   review.value(QLatin1StringView("rating")).toInt() / 10,
+                                   usefulTotal,
+                                   usefulFavorable,
+                                   usefulWilson,
+                                   reviewVersion));
+            // We can also receive just a json with app name and user info so filter these out as there is no review
+            if (!r->summary().isEmpty() && !r->reviewText().isEmpty()) {
+                reviewsList.append(r);
+                // Needed for submitting usefulness
+                r->addMetadata(QLatin1StringView("ODRS::user_skey"), review.value(QLatin1StringView("user_skey")).toString());
+            }
+
+            // We should get at least user_skey needed for posting reviews
+            m_resource->addMetadata(QLatin1StringView("ODRS::user_skey"), review.value(QLatin1StringView("user_skey")).toString());
+        }
     }
+
+    Q_EMIT reviewsReady(reviewsList, false);
 }
