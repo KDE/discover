3f21df4ba47c6f8f1a57e53e62c9bcfe4beeff4c
Jeremy Whiting <jpwhiting@kde.org>
2025-04-04 11:02:55 -0600

    Also enable steamos backend when building in CI for tests.

--- libdiscover/backends/SteamOSBackend/tests/mock_server.cpp
+++ libdiscover/backends/SteamOSBackend/tests/mock_server.cpp
@@ -1,6 +1,6 @@
 /***************************************************************************
- *   SPDX-FileCopyrightText: 2024 Jeremy Whiting <jpwhiting@kde.org>       *
- *                                                                         *
+ *   SPDX-FileCopyrightText: 2024 Jeremy Whiting <jpwhiting@kde.org>
+ *
  *   SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
  ***************************************************************************/
 
