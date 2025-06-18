KDE/discover

commit f808988e2ab57b95fb2ffe9b53cfc2b29f92ee8a

ApplicationPage: allow offline update header to take up enough space
We were only setting a maximum width here, which allowed wrapping to
work properly. But we weren't actually filling that width, so the layout
didn't get big enough when it had very long text.

BUG: 504513
FIXED-IN: 6.4.0

discover/qml/ApplicationPage.qml
@@ -291,7 +291,7 @@ DiscoverPage {
                 // App icon, name, author, and rating
                 RowLayout {
                     id: appBasicInfoLayout
-                    Layout.maximumWidth: headerLayout.implicitWidth
+                    Layout.fillWidth: true
                     Layout.alignment: headerLayout.stackedMode ? Qt.AlignHCenter : Qt.AlignLeft
                     spacing: appInfo.padding
 

commit 27d483ab4fb2019b619f399defdc3dc132db2986

ApplicationPage: fix uncentered app icon/name in stacked mode
This regressed in f808988e2ab57b95fb2ffe9b53cfc2b29f92ee8a. This change
keeps the fix from that commit, but avoids regressing the display of
app names in stacked mode.

discover/qml/ApplicationPage.qml
@@ -292,6 +292,7 @@ DiscoverPage {
                 RowLayout {
                     id: appBasicInfoLayout
                     Layout.fillWidth: true
+                    Layout.maximumWidth: headerLayout.stackedMode ? headerLayout.implicitWidth : -1
                     Layout.alignment: headerLayout.stackedMode ? Qt.AlignHCenter : Qt.AlignLeft
                     spacing: appInfo.padding
 