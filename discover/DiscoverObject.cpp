8b3229a431808c05edf6c8c31f7d049927b81a67
Nate Graham <nate@kde.org>
2025-10-03 14:04:33 -0600

    Use safer org.kde.Shutdown to shut down and restart
    Currently we use org.freedesktop.login1, which will not let apps prompt
    the user to save changes. This can lead to data loss.
    
    Ideally org.freedesktop.login1 would be plumbed through our session
    stuff so apps can prompt, but that's quite a ways away. In the meantime,
    use org.kde.Shutdown to resolve the issue.
    
    BUG: 499260
    FIXED-IN: 6.4.6
    
    
    (cherry picked from commit 025dc056076be0c54a50dc988c18ab2adb439812)
    
    Co-authored-by: Nate Graham <nate@kde.org>

--- discover/DiscoverObject.cpp
+++ discover/DiscoverObject.cpp
@@ -675,12 +675,11 @@ void DiscoverObject::promptReboot()
 void DiscoverObject::rebootNow()
 {
     setAboutToReboot();
-    auto method = QDBusMessage::createMethodCall(QStringLiteral("org.freedesktop.login1"),
-                                                 QStringLiteral("/org/freedesktop/login1"),
-                                                 QStringLiteral("org.freedesktop.login1.Manager"),
-                                                 QStringLiteral("Reboot"));
-    method.setArguments({true /*interactive*/});
-    QDBusConnection::systemBus().asyncCall(method);
+    auto method = QDBusMessage::createMethodCall(QStringLiteral("org.kde.Shutdown"),
+                                                 QStringLiteral("/Shutdown"),
+                                                 QStringLiteral("org.kde.Shutdown"),
+                                                 QStringLiteral("logoutAndReboot"));
+    QDBusConnection::sessionBus().asyncCall(method);
 }
 
 void DiscoverObject::shutdownNow()
@@ -694,12 +693,11 @@ void DiscoverObject::shutdownNow()
     }
     setAboutToPowerOff();
 
-    auto method = QDBusMessage::createMethodCall(QStringLiteral("org.freedesktop.login1"),
-                                                 QStringLiteral("/org/freedesktop/login1"),
-                                                 QStringLiteral("org.freedesktop.login1.Manager"),
-                                                 shouldRebootFirst ? QStringLiteral("Reboot") : QStringLiteral("PowerOff"));
-    method.setArguments({true /*interactive*/});
-    QDBusConnection::systemBus().asyncCall(method);
+    auto method = QDBusMessage::createMethodCall(QStringLiteral("org.kde.Shutdown"),
+                                                 QStringLiteral("/Shutdown"),
+                                                 QStringLiteral("org.kde.Shutdown"),
+                                                 shouldRebootFirst ? QStringLiteral("logoutAndReboot") : QStringLiteral("logoutAndShutdown"));
+    QDBusConnection::sessionBus().asyncCall(method);
 }
 
 QString DiscoverObject::describeSources() const
