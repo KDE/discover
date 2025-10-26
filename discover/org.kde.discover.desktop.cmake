776ac79c9656204a67c087cf15b439d42c03fbde
l10n daemon script <scripty@kde.org>
2025-10-26 03:12:15 +0000

    SVN_SILENT made messages (.desktop file) - always resolve ours
    In case of conflict in i18n, keep the version of the branch "ours"
    To resolve a particular conflict, "git checkout --ours path/to/file.desktop"

--- discover/org.kde.discover.desktop.cmake
+++ discover/org.kde.discover.desktop.cmake
@@ -62,6 +62,7 @@ Comment[ar]=ثبت وأزل الحزم والملحقات
 Comment[bg]=Инсталиране и премахване на приложения и приставки
 Comment[ca]=Instal·la i elimina aplicacions i complements
 Comment[ca@valencia]=Instal·la i elimina aplicacions i complements
+Comment[cs]=Instaluje a odebírá aplikace či doplňky
 Comment[de]=Anwendungen und Erweiterungen installieren und entfernen
 Comment[eo]=Instali kaj forigi apojn kaj kromaĵojn
 Comment[es]=Instalar y eliminar aplicaciones y complementos
